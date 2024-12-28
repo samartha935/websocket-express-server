5ad4da0057cec026de47119b1776375fe26da443
samartha935 <147503219+samartha935@users.noreply.github.com>
2024-12-28 23:25:27 +0530

    created sensor data server

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,101 @@
 "use strict";
-// require("dotenv").config();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = require("http");
+const ws_1 = require("ws");
+const API_KEY = "123123";
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+//This is just for example.
+app.get("/get", (req, res) => {
+    console.log("get activateed");
+    res.send("get activateed");
+});
+const server = (0, http_1.createServer)(app);
+const AudioStreamWss = new ws_1.WebSocketServer({ noServer: true });
+const SensorDataWss = new ws_1.WebSocketServer({ noServer: true });
+const clientMetaData = new Map();
+SensorDataWss.on("connection", (ws, request) => {
+    console.log("client connected.");
+    ws.on("error", (e) => {
+        console.log(e);
+    });
+    ws.on("message", (data) => {
+        //  Add Throttling for the data broadcast.
+        var _a;
+        const params = new URLSearchParams(new URL(request.url, `https://${request.headers.host}`).searchParams);
+        const clientKey = params.get("apikey");
+        const isAuthorized = API_KEY === clientKey;
+        clientMetaData.set(ws, { isAuthorized });
+        if ((_a = clientMetaData.get(ws)) === null || _a === void 0 ? void 0 : _a.isAuthorized) {
+            if (Buffer.isBuffer(data)) {
+                // Convert buffer to string
+                const decodedMessage = data.toString();
+                console.log(`Message received: ${decodedMessage}`);
+                SensorDataWss.clients.forEach((client) => {
+                    if (client.readyState === ws_1.WebSocket.OPEN) {
+                        client.send(decodedMessage);
+                    }
+                });
+            }
+            else {
+                // Already a string
+                console.log("Message recieved : ", data);
+                SensorDataWss.clients.forEach((client) => {
+                    if (client.readyState === ws_1.WebSocket.OPEN) {
+                        client.send(data);
+                    }
+                });
+            }
+        }
+        else {
+            ws.send("You are not authorized to broadcast data.");
+            return;
+        }
+    });
+    ws.on("close", (code, reason) => {
+        clientMetaData.delete(ws);
+        console.log("Client disconnected ", code, reason);
+    });
+});
+AudioStreamWss.on("connection", (ws) => {
+    console.log("client connected.");
+    ws.on("error", (e) => {
+        console.log(e);
+    });
+    ws.on("message", (data) => {
+        console.log(data);
+    });
+    ws.on("close", (code, reason) => {
+        console.log("Client disconnected ", code, reason);
+    });
+});
+server.on("upgrade", (request, socket, head) => {
+    const pathname = new URL(request.url, `https://${request.headers.host}`)
+        .pathname;
+    switch (pathname) {
+        case "/audio-stream":
+            AudioStreamWss.handleUpgrade(request, socket, head, (ws) => {
+                AudioStreamWss.emit("connection", ws, request);
+            });
+            break;
+        case "/sensor-data":
+            SensorDataWss.handleUpgrade(request, socket, head, (ws) => {
+                SensorDataWss.emit("connection", ws, request);
+            });
+            break;
+        default:
+            socket.destroy();
+            console.log("Invalid pathname");
+    }
+});
+server.listen(3000, () => {
+    console.log("server started");
+});
+// require("dotenv").config();
 // import express from "express";
 // import { WebSocketServer, WebSocket } from "ws";
 // const app = express();
@@ -40,79 +132,3 @@ Object.defineProperty(exports, "__esModule", { value: true });
 //     }
 //   });
 // });
-const express_1 = __importDefault(require("express"));
-const http_1 = require("http");
-const ws_1 = require("ws");
-const app = (0, express_1.default)();
-app.use(express_1.default.json());
-app.get("/get", (req, res) => {
-    console.log("get activateed");
-    res.send("get activateed");
-});
-const server = (0, http_1.createServer)(app);
-const AudioStreamWss = new ws_1.WebSocketServer({ noServer: true });
-const SensorDataWss = new ws_1.WebSocketServer({ noServer: true });
-AudioStreamWss.on("connection", (ws) => {
-    console.log("client connected.");
-    ws.on("error", (e) => {
-        console.log(e);
-    });
-    ws.on("message", (data) => {
-        console.log(data);
-    });
-    ws.on("close", (code, reason) => {
-        console.log("Client disconnected ", code, reason);
-    });
-});
-SensorDataWss.on("connection", (ws) => {
-    console.log("client connected.");
-    ws.on("error", (e) => {
-        console.log(e);
-    });
-    ws.on("message", (data) => {
-        if (Buffer.isBuffer(data)) {
-            // Convert buffer to string
-            const decodedMessage = data.toString();
-            console.log(`Message received: ${decodedMessage}`);
-            SensorDataWss.clients.forEach((client) => {
-                if (client.readyState === ws_1.WebSocket.OPEN) {
-                    client.send(decodedMessage);
-                }
-            });
-        }
-        else {
-            // Already a string
-            console.log("Message recieved : ", data);
-            SensorDataWss.clients.forEach((client) => {
-                if (client.readyState === ws_1.WebSocket.OPEN) {
-                    client.send(data);
-                }
-            });
-        }
-    });
-    ws.on("close", (code, reason) => {
-        console.log("Client disconnected ", code, reason);
-    });
-});
-server.on("upgrade", (request, socket, head) => {
-    const pathname = new URL(request.url, `https://${request.headers.host}`)
-        .pathname;
-    switch (pathname) {
-        case "/audio-stream":
-            AudioStreamWss.handleUpgrade(request, socket, head, (ws) => {
-                AudioStreamWss.emit("connection", ws, request);
-            });
-            break;
-        case "/sensor-data":
-            SensorDataWss.handleUpgrade(request, socket, head, (ws) => {
-                SensorDataWss.emit("connection", ws, request);
-            });
-            break;
-        default:
-            socket.destroy();
-            console.log("Invalid pathname");
-    }
-});
-server.listen(3000, () => {
-    console.log("server started");
-});
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,119 @@
+import express from "express";
+import { createServer } from "http";
+import { WebSocketServer, WebSocket } from "ws";
+
+const API_KEY = "123123";
+
+const app = express();
+app.use(express.json());
+
+//This is just for example.
+app.get("/get", (req, res) => {
+  console.log("get activateed");
+  res.send("get activateed");
+});
+
+const server = createServer(app);
+
+const AudioStreamWss = new WebSocketServer({ noServer: true });
+const SensorDataWss = new WebSocketServer({ noServer: true });
+
+const clientMetaData = new Map<WebSocket, { isAuthorized: boolean }>();
+
+SensorDataWss.on("connection", (ws, request) => {
+  console.log("client connected.");
+
+  ws.on("error", (e) => {
+    console.log(e);
+  });
+
+  ws.on("message", (data) => {
+    //  Add Throttling for the data broadcast.
+
+    const params = new URLSearchParams(
+      new URL(request.url!, `https://${request.headers.host}`).searchParams
+    );
+
+    const clientKey = params.get("apikey");
+
+    const isAuthorized = API_KEY === clientKey;
+
+    clientMetaData.set(ws, { isAuthorized });
+
+    if (clientMetaData.get(ws)?.isAuthorized) {
+      if (Buffer.isBuffer(data)) {
+        // Convert buffer to string
+        const decodedMessage = data.toString();
+        console.log(`Message received: ${decodedMessage}`);
+        SensorDataWss.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(decodedMessage);
+          }
+        });
+      } else {
+        // Already a string
+        console.log("Message recieved : ", data);
+        SensorDataWss.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(data);
+          }
+        });
+      }
+    } else {
+      ws.send("You are not authorized to broadcast data.");
+      return;
+    }
+  });
+
+  ws.on("close", (code, reason) => {
+    clientMetaData.delete(ws);
+    console.log("Client disconnected ", code, reason);
+  });
+});
+
+AudioStreamWss.on("connection", (ws) => {
+  console.log("client connected.");
+
+  ws.on("error", (e) => {
+    console.log(e);
+  });
+
+  ws.on("message", (data) => {
+    console.log(data);
+  });
+
+  ws.on("close", (code, reason) => {
+    console.log("Client disconnected ", code, reason);
+  });
+});
+
+server.on("upgrade", (request, socket, head) => {
+  const pathname = new URL(request.url!, `https://${request.headers.host}`)
+    .pathname;
+
+  switch (pathname) {
+    case "/audio-stream":
+      AudioStreamWss.handleUpgrade(request, socket, head, (ws) => {
+        AudioStreamWss.emit("connection", ws, request);
+      });
+      break;
+
+    case "/sensor-data":
+      SensorDataWss.handleUpgrade(request, socket, head, (ws) => {
+        SensorDataWss.emit("connection", ws, request);
+      });
+      break;
+
+    default:
+      socket.destroy();
+      console.log("Invalid pathname");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("server started");
+});
+
 // require("dotenv").config();
 
 // import express from "express";
@@ -46,96 +162,3 @@
 //     }
 //   });
 // });
-
-import express from "express";
-import { createServer } from "http";
-import { WebSocketServer, WebSocket } from "ws";
-
-const app = express();
-app.use(express.json());
-
-app.get("/get", (req, res) => {
-  console.log("get activateed");
-  res.send("get activateed");
-});
-
-const server = createServer(app);
-
-const AudioStreamWss = new WebSocketServer({ noServer: true });
-const SensorDataWss = new WebSocketServer({ noServer: true });
-
-SensorDataWss.on("connection", (ws) => {
-  console.log("client connected.");
-
-  ws.on("error", (e) => {
-    console.log(e);
-  });
-
-  ws.on("message", (data) => {
-    if (Buffer.isBuffer(data)) {
-      // Convert buffer to string
-      const decodedMessage = data.toString();
-      console.log(`Message received: ${decodedMessage}`);
-      SensorDataWss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(decodedMessage);
-        }
-      });
-    } else {
-      // Already a string
-      console.log("Message recieved : ", data);
-      SensorDataWss.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(data);
-        }
-      });
-    }
-  });
-
-  ws.on("close", (code, reason) => {
-    console.log("Client disconnected ", code, reason);
-  });
-});
-
-AudioStreamWss.on("connection", (ws) => {
-  console.log("client connected.");
-
-  ws.on("error", (e) => {
-    console.log(e);
-  });
-
-  ws.on("message", (data) => {
-    console.log(data);
-  });
-
-  ws.on("close", (code, reason) => {
-    console.log("Client disconnected ", code, reason);
-  });
-});
-
-server.on("upgrade", (request, socket, head) => {
-  const pathname = new URL(request.url!, `https://${request.headers.host}`)
-    .pathname;
-
-  switch (pathname) {
-    case "/audio-stream":
-      AudioStreamWss.handleUpgrade(request, socket, head, (ws) => {
-        AudioStreamWss.emit("connection", ws, request);
-      });
-      break;
-
-    case "/sensor-data":
-      SensorDataWss.handleUpgrade(request, socket, head, (ws) => {
-        SensorDataWss.emit("connection", ws, request);
-      });
-      break;
-
-    default:
-      socket.destroy();
-      console.log("Invalid pathname");
-  }
-});
-
-server.listen(3000, () => {
-  console.log("server started");
-});
